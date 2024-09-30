db63479aebdb971e782b127a7887f5a3fc2f33b5
aiwithbenefits <jacob@aiwithbenefits.com>
2024-09-30 01:32:18 -0400

    Update StartCall.tsx

--- components/StartCall.tsx
+++ components/StartCall.tsx
@@ -2,6 +2,8 @@ import { useVoice } from "@humeai/voice-react";
 import { AnimatePresence, motion } from "framer-motion";
 import { Button } from "./ui/button";
 import { Phone } from "lucide-react";
+import * as Hume from "../../../../../../index";
+import 
 
       {
           name: "EVI 2 config",
@@ -34,7 +36,35 @@ import { Phone } from "lucide-react";
               }
           }
       }
-
+export interface PostedConfig {
+    /** Specifies the EVI version to use. Use `"1"` for version 1, or `"2"` for the latest enhanced version. For a detailed comparison of the two versions, refer to our [guide](/docs/empathic-voice-interface-evi/evi-2). */
+    eviVersion: string;
+    /** Name applied to all versions of a particular Config. */
+    name: string;
+    /** An optional description of the Config version. */
+    versionDescription?: string;
+    prompt?: Hume.empathicVoice.PostedConfigPromptSpec;
+    /** A voice specification associated with this Config. */
+    voice?: Hume.empathicVoice.PostedVoice;
+    /**
+     * The supplemental language model associated with this Config.
+     *
+     * This model is used to generate longer, more detailed responses from EVI. Choosing an appropriate supplemental language model for your use case is crucial for generating fast, high-quality responses from EVI.
+     */
+    languageModel?: Hume.empathicVoice.PostedLanguageModel;
+    /**
+     * The eLLM setup associated with this Config.
+     *
+     * Hume's eLLM (empathic Large Language Model) is a multimodal language model that takes into account both expression measures and language. The eLLM generates short, empathic language responses and guides text-to-speech (TTS) prosody.
+     */
+    ellmModel?: Hume.empathicVoice.PostedEllmModel;
+    /** List of user-defined tools associated with this Config. */
+    tools?: (Hume.empathicVoice.PostedUserDefinedToolSpec | undefined)[];
+    /** List of built-in tools associated with this Config. */
+    builtinTools?: (Hume.empathicVoice.PostedBuiltinTool | undefined)[];
+    eventMessages?: Hume.empathicVoice.PostedEventMessageSpecs;
+    timeouts?: Hume.empathicVoice.PostedTimeoutSpecs;
+}
 export default function StartCall() {
   const { status, connect } = useVoice(config_id="e2c38ac3-e8bc-4a6f-818d-d86e6d924a91");
 
